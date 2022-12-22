55c140a1fbe8ce10f9716dec6c234b37c9886bb0
Hugo Kooi <hugo.kooi@hva.nl>
2022-12-22 01:37:05 +0100

    chore: added ESLint and prettier and fixed errors by changing eslintrc to athom/homey-app

--- app.ts
+++ app.ts
@@ -1,14 +1,14 @@
-import Homey from 'homey';
+"use strict";
+
+import Homey from "homey";
 
 class MyApp extends Homey.App {
-
-  /**
-   * onInit is called when the app is initialized.
-   */
-  async onInit() {
-    this.log('MyApp has been initialized');
-  }
-
+    /**
+     * onInit is called when the app is initialized.
+     */
+    async onInit() {
+        this.log("MyApp has been initialized");
+    }
 }
 
 module.exports = MyApp;
